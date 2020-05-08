a6508ba5baa4d792402b2d8e9adc470a10f44d2f
sarah-link <slink2647@gmail.com>
2020-05-07 19:29:23 -0500

    start

--- src/App.js
+++ src/App.js
@@ -2,25 +2,50 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+class HamburgerMenu extends React.Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+function Header() {
+    return(
+        <header className={"app-header"} >
+            <Button text={"Sign Up"} className={"header-button"} />
+            <Button text={"Log In"} className={"header-button"} />
+        </header>
+    )
+}
+
+function Button(props) {
+    return(
+        <a className={props.className} href="/">
+            {props.text}
         </a>
-      </header>
-    </div>
-  );
+    )
+}
+
+
+function App() {
+    return (
+        <div className="App">
+            <Header />
+            <div className="App-body">
+                <h1>
+                    <b>init</b>
+                </h1>
+
+                <h4>
+                    A better way to roll initiative or something idk
+                </h4>
+
+                <Button text={"CREATE ENCOUNTER"} className={"main-button"} />
+                <Button text={"MANAGE LIBRARY"} className={"main-button"} />
+            </div>
+        </div>
+    );
 }
 
 export default App;
