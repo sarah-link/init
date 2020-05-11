f64737318282d3009bef4868beae392ed04a1bcb
Emma Lucas <edmmalucas256@gmail.com>
2020-05-10 21:28:02 -0500

    css color changes and small tweaks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 
-function Header() {
+function NavBar() {
     return(
-        <header className={"app-header"} >
+        <header className={"app-navbar"} >
             <div>
-                <Button text={"Sign Up"} className={"header-button"} />
-                <Button text={"Log In"} className={"header-button"} />
+                <Button text={"Sign Up"} className={"navbar-button"} />
+                <Button text={"Log In"} className={"navbar-button"} />
             </div>
             <SideNav />
 
@@ -24,7 +24,7 @@ function Button(props) {
 
 function IconButton(props) {
     return (
-            <svg id="menu-button" width="32" height="32" viewBox="0 0 20 20" fill="black" onClick={props.onClick}
+            <svg id="menu-button" width="40" height="40" viewBox="0 0 20 20" onClick={props.onClick}
                  xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd"
                       d="M6.646 3.646a.5.5 0 01.708 0l6 6a.5.5 0 010 .708l-6 6a.5.5 0 01-.708-.708L12.293 10 6.646 4.354a.5.5 0 010-.708z"
@@ -42,18 +42,21 @@ class SideNav extends React.Component {
         console.log("hek")
         if (this.state.isOpen == true) {
             document.getElementById("mySidenav").style.left = "0px";
+            document.getElementById("mySidenav").style.boxShadow = "0px 0px 50px rgba(0,0,0,0.5)";
             document.getElementById("menu-button").style.left = "410px";
+            document.getElementById("menu-button").style.fill = "#EFFCF6";
         } else {
             document.getElementById("mySidenav").style.left = "-450px";
+            document.getElementById("mySidenav").style.boxShadow = "0px 0px 20px rgba(0,0,0,0)";
             document.getElementById("menu-button").style.left = "0px";
-
+            document.getElementById("menu-button").style.fill = "#147D64";
         }
         this.setState({isOpen: !this.state.isOpen})
     }
     render() {
         return (
             <div id={"mySidenav"}>
-                <IconButton className={"header-button"} onClick={() => this.toggleSidebar()} />
+                <IconButton className={"navbar-button"} onClick={() => this.toggleSidebar()} />
             </div>
 
         )
@@ -63,18 +66,21 @@ class SideNav extends React.Component {
 function App() {
     return (
         <div className="App">
-            <Header />
+            <NavBar />
             <div className="App-body">
-                <h1>
-                    <b>rollinit</b>
-                </h1>
+                <div className="App-header">
+                    <h1>
+                        <b>rollinit</b>
+                    </h1>
 
-                <h4>
-                    A better way to roll initiative or something idk
-                </h4>
+                    <h4>
+                        A better way to roll initiative or something idk
+                    </h4>
+                </div>
 
                 <Button text={"CREATE ENCOUNTER"} className={"main-button"} />
                 <Button text={"MANAGE LIBRARY"} className={"main-button"} />
+                <Button text={"JOIN ENCOUNTER"} className={"main-button"} />
             </div>
         </div>
     );
